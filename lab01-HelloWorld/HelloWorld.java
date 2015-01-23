2701bb4f9322430edd45ae738ed74133c77d4f71
dgr216 <dgr216@lehigh.edu>
2015-01-23 16:34:16 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+//Daniel Reilly
+//Hello World Java Program
+
+
+//define a class
+public class HelloWorld{
+    
+    //add a main method
+    public static void main(String[] args){
+        
+        //print the statement with box around it
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2015!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+    }
+}
