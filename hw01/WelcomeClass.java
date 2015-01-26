f41d7d799508dec43afc9fa217f3020b5edbdd25
dgr216 <dgr216@lehigh.edu>
2015-01-26 18:49:09 +0000

    updated hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//Daniel Reilly
+//1/25/15
+//CSE002 hw01
+//Welcome Class Java Program
+//Displays to the screen a welcome from me 
+//Using my Lehigh network ID as a signature
+
+//define a class
+public class WelcomeClass{
+    
+    //add a main method
+    public static void main(String[] args){
+        
+        //print the statement with box around it
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-D--G--R--2--1--6->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v" );
+        System.out.println("I'm a senior Bioengineer on the Cross Country and Track team.");
+    }
+}
